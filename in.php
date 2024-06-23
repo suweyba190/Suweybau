b2b18ebd5bee08d760f829e09f10c85f19c9346b
suweyba190 <ahmed.suweyba@strathmore.edu>
2024-06-23 10:01:08 +0300

    store php

--- in.php
+++ in.php
@@ -5,4 +5,17 @@
     print date('1');//ddisplaying the current day of week
     print "<br>";
     print date('d/F/Y',strtotime('+2 months'));
+    //Creating a (declaring) a variable
+    $fname ="alex kama";//declaration of a variable or a string  or a group of words
+    $yob = 1999;//declaring of a digit or an interger
+    print "<br>";
+    echo $yob;
+    print"<br>";
+    $current_year = date('Y');
+    echo $current_year;
+     print"<br>";
+     $age = $current_year - $yob;
+
+
+
     ?>
