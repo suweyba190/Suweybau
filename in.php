8f88e66d309459e232664dad89436d07a914de1f
suweyba190 <ahmed.suweyba@strathmore.edu>
2024-06-23 09:46:03 +0300

    push

---
+++ in.php
@@ -0,0 +1,8 @@
+   <?php
+    print "this is my first php code"; 
+    print "<br>";//displaying a sentence or a string 
+    print " today is<span style='color: #ff4856; text-transform:uppercase'> thursday </span>";
+    print date('1');//ddisplaying the current day of week
+    print "<br>";
+    print date('d/F/Y',strtotime('+2 months'));
+    ?>
