86d6fe94ae97332bb2e378b7388d9ae92ef73d29
suweyba190 <ahmed.suweyba@strathmore.edu>
2024-06-28 11:29:45 +0300

    push

---
+++ templates/nav.php
@@ -0,0 +1,11 @@
+<div class="nav-links">
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                    <li><a href="project.html">Project</a></li>
+                    <li><a href="solution.html">Solution</a></li>
+                    <li><a href="team.html">Team</a></li>
+                    <li><a href="blog">Blog</a></li>
+                </ul>
+            </div>
